a2e1ca49665e9eb8b3e66780dedfd25491de2b3e
Zheng Jianing <jianing.zheng@rea-group.com>
2013-10-17 19:52:04 +0800

    play sound example

--- ViocePlay/VPViewController.m
+++ ViocePlay/VPViewController.m
@@ -7,23 +7,69 @@
 //
 
 #import "VPViewController.h"
+#import "AudioToolbox/AudioToolbox.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface VPViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *soundButton;
 @end
 
 @implementation VPViewController
+{
+    SystemSoundID shortSound;
+    AVAudioPlayer *audioPlayer;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+//    NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"hello-wav" ofType:@"wav"];
+//    if (soundPath) {
+//        NSURL *soundURL = [NSURL fileURLWithPath:soundPath];
+//        OSStatus err = AudioServicesCreateSystemSoundID((__bridge CFURLRef)soundURL, &shortSound);
+//        if (err != kAudioServicesNoError) {
+//            NSLog(@"-------- Could not load %@, error code: %ld", soundURL, err);
+//        }
+//    }
+    
+    NSString *musicPath = [[NSBundle mainBundle] pathForResource:@"hello"
+                                                          ofType:@"aiff"];
+    if (musicPath) {
+        NSURL *musicURL = [NSURL fileURLWithPath:musicPath];
+        audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:musicURL
+                                                             error:nil];
+        [audioPlayer setDelegate:self];
+    }
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)playSound:(UIButton *)sender {
+//    AudioServicesPlaySystemSound(shortSound);
+//    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    
+    if ([audioPlayer isPlaying]) {
+        [audioPlayer stop];
+        [sender setTitle:@"点击播放"
+                forState:UIControlStateNormal];
+    }
+    else {
+        [audioPlayer play];
+        [sender setTitle:@"停止"
+                forState:UIControlStateNormal];
+    }
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player
+                       successfully:(BOOL)flag
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.soundButton setTitle:@"点击播放"
+                 forState:UIControlStateNormal];
+}
+
+- (void)audioPlayerEndInterruption:(AVAudioPlayer *)player
+{
+    [audioPlayer play];
 }
 
 @end
